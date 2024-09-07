9d04ac913232ecfb8129a3031428cfcb365d5ca3
Kuro Cybria <rizqikurniawan1863@gmail.com>
2024-09-07 22:14:29 +0700

    Add show full galleries feature

--- frontend/src/app/projects/[slug]/content/page.jsx
+++ frontend/src/app/projects/[slug]/content/page.jsx
@@ -1,17 +1,19 @@
 'use client';
-import React from 'react'
-import { redirect } from 'next/navigation'
+import React, { useRef, useState, createContext } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { EffectCoverflow, Navigation } from 'swiper/modules'
+import { redirect } from 'next/navigation'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/effect-coverflow'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCoverflow, Navigation } from 'swiper/modules'
+import { AiFillOpenAI, AiFillEye } from "react-icons/ai";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import ShowFullGalleries from '@/components/showFullGalleries';
 import MainContent from '@/components/mainContent'
 import projects from '@/data/projects.json'
-import { AiFillOpenAI } from "react-icons/ai";
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+
 
 // export async function generateStaticParams({ params }) {
 //   try {
@@ -30,7 +32,11 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 //   }
 // }
 
+export const GalleryContext = createContext()
+
 const page = ({ params }) => {
+  const [showGalleries, setShowGalleries] = useState(false)
+  const swiperRef = useRef()
   const project = projects.find(project => project.slug === params.slug)
 
   if (!project) {
@@ -38,115 +44,147 @@ const page = ({ params }) => {
   }
 
   return (
-    <MainContent title="Projects">
-      <div className="flex flex-col px-6 md:px-12 pb-10">
-        <div className="flex flex-col items-center pt-5 ">
-          <h1 className="h-1 text-3xl md:text-4xl font-bold">{project.title}</h1>
-        </div>
-        <div className="relative overflow-visible">
-          <div className="absolute inset-0 bg-gray-200 blur mx-5 md:mx-16 lg:mx-32 mb-10 mt-20"></div>
-          <span className="block relative">
-            <Image
-              src={project.assets[0]}
-              alt={project.title}
-              width={1920}
-              height={1080}
-              layout="responsive"
-              className="px-5 md:px-16 lg:px-32 pb-10 pt-20"
-            />
-          </span>
-        </div>
-
-        <p className="text-lg text-justify whitespace-pre-wrap">{project.description}</p>
-
-        <div className="py-12">
-          <p className="text-md text-justify whitespace-pre-wrap pb-2">Credits</p>
-          <ul className="list-disc px-4">
-            {
-              project.credits ?
-                project.credits.map((credit, index) => {
-                  return (
-                    <li key={index} className="text-md whitespace-pre-wrap">
-                      <div className="flex items-center">
-                        <span className="text-justify">
-                          <Link href={credit.github} target="_blank">
-                            <p className="text-md text-white hover:text-[#FF00CD] active:text-[#FFD700]">{credit.name}</p>
-                          </Link>
-                        </span>
-                      </div>
-                    </li>
-                  )
-                })
-                :
-                <li className="text-md">None</li>
-            }
-          </ul>
-          <div className="flex pt-10 gap-x-2">
-            <p className="text-md">Link/Url&apos;s:</p>
-            {
-              project.status === 'Active' ?
-                <Link href={project.url}>
-                  <p className="text-md text-white">{project.url}</p>
-                </Link>
-                :
-                <p className="text-md text-gray-400">
-                  <del>{project.url}</del> {project.status}!
-                </p>
-
-            }
+    <>
+      <MainContent title="Projects">
+        <div className="flex flex-col px-6 md:px-12 pb-10">
+          <div className="flex flex-col items-center pt-5 ">
+            <h1 className="h-1 text-3xl md:text-4xl font-bold">{project.title}</h1>
           </div>
-        </div>
-        <div className="flex flex-col items-center py-5">
-          <div className="animate-spin" style={{ animationDuration: '10s' }}>
-            <AiFillOpenAI size={30} />
+          <div className="relative overflow-visible">
+            <div className="absolute inset-0 bg-gray-200 blur mx-5 md:mx-16 lg:mx-32 mb-10 mt-20"></div>
+            <span className="block relative">
+              <Image
+                src={project.assets[0]}
+                alt={project.title}
+                width={1920}
+                height={1080}
+                layout="responsive"
+                className="px-5 md:px-16 lg:px-32 pb-10 pt-20"
+              />
+            </span>
           </div>
-          <p className="text-center text-2xl font-semibold pt-2">Galleries</p>
-        </div>
-        <div className="flex flex-col items-center">
-          <span className="w-6/12 bg-gradient-to-r from-transparent via-gray-500 to-transparent h-0.5 my-3"></span>
-        </div>
 
-        {/* Image Slider */}
-        <div className="flex w-full justify-center items-center pt-5 gap-x-5">
-          <FaChevronLeft size={30} />
-          <span className='w-9/12 md:w-10/12 lg:w-7/12 h-full bg-gradient-to-r from-inherit via-transparent via-[percentage:20%_80%] to-inherit'>
-            <Swiper
-              // navigation
-              effect="coverflow"
-              modules={[EffectCoverflow, Navigation]}
-              loop={true}
-              breakpoints={{
-                640: {
-                  slidesPerView: 1,
-                },
-                768: {
-                  slidesPerView: 3,
-                },
-              }}
-              className="h-full swiper-container"
-            >
+          <p className="text-lg text-justify whitespace-pre-wrap">{project.description}</p>
+
+          <div className="py-12">
+            <p className="text-md text-justify whitespace-pre-wrap pb-2">Credits</p>
+            <ul className="list-disc px-4">
               {
-                project.assets.map((asset, index) => {
-                  return (
-                    <SwiperSlide key={index}>
-                      <Image
-                        src={asset}
-                        alt={project.title}
-                        width={1920}
-                        height={1080}
-                        layout="cover"
-                        className="h-full w-full object-cover"
-                      />
-                    </SwiperSlide>
-                  )
-                })
+                project.credits ?
+                  project.credits.map((credit, index) => {
+                    return (
+                      <li key={index} className="text-md whitespace-pre-wrap">
+                        <div className="flex items-center">
+                          <span className="text-justify">
+                            <Link href={credit.github} target="_blank">
+                              <p className="text-md text-white hover:text-[#FF00CD] active:text-[#FFD700]">{credit.name}</p>
+                            </Link>
+                          </span>
+                        </div>
+                      </li>
+                    )
+                  })
+                  :
+                  <li className="text-md">None</li>
               }
-            </Swiper>
-          </span>
-          <FaChevronRight size={30} />
+            </ul>
+            <div className="flex pt-10 gap-x-2">
+              <p className="text-md">Link/Url&apos;s:</p>
+              {
+                project.status === 'Active' ?
+                  <Link href={project.url}>
+                    <p className="text-md text-white">{project.url}</p>
+                  </Link>
+                  :
+                  <p className="text-md text-gray-400">
+                    <del>{project.url}</del> {project.status}!
+                  </p>
+
+              }
+            </div>
+          </div>
+          <div className="flex flex-col items-center py-5">
+            <div className="animate-spin" style={{ animationDuration: '10s' }}>
+              <AiFillOpenAI size={30} />
+            </div>
+            <p className="text-center text-2xl font-semibold pt-2">Galleries</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="w-6/12 bg-gradient-to-r from-transparent via-gray-500 to-transparent h-0.5 my-3"></span>
+          </div>
+
+          {/* Image Slider */}
+          <div className="flex w-full justify-center items-center pt-5 gap-x-5">
+            <button onClick={() => swiperRef.current.slidePrev()}>
+              <FaChevronLeft size={30} color="white" />
+            </button>
+            <span className='w-9/12 md:w-10/12 lg:w-7/12 h-full'>
+              <Swiper
+                // navigation
+                effect="coverflow"
+                modules={[EffectCoverflow, Navigation]}
+                grabCursor={true}
+                loop={true}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                  },
+                  768: {
+                    slidesPerView: 3,
+                  },
+                }}
+                navigation={{
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                  clickable: true,
+                }}
+                className="h-full swiper-container"
+                onSwiper={(swiper) => (swiperRef.current = swiper)}
+              >
+                {
+                  project.assets.map((asset, index) => {
+                    return (
+                      <SwiperSlide key={index}>
+                        <Image
+                          src={asset}
+                          alt={project.title}
+                          width={1920}
+                          height={1080}
+                          layout="cover"
+                          className="h-full w-full object-cover"
+                          loading="lazy"
+                        />
+                      </SwiperSlide>
+                    )
+                  })
+                }
+
+
+              </Swiper>
+            </span>
+            <button onClick={() => swiperRef.current.slideNext()}>
+              <FaChevronRight size={30} color="white" />
+            </button>
+
+          </div>
+
+          <div className="flex items-center justify-center mt-10 ">
+            <button
+              className="flex items-center justify-center p-2 rounded-md bg-[#313131] gap-x-1 border border-green-500 hover:bg-green-500 transition-colors duration-200  focus:border-green-600 focus:bg-green-600"
+              onClick={() => setShowGalleries(true)}
+            >
+              <AiFillEye size={20} color='white' />
+              <p className="text-sm font-bold">Show All</p>
+            </button>
+          </div>
         </div>
-      </div>
-    </MainContent>
+      </MainContent>
+      <GalleryContext.Provider value={[showGalleries, setShowGalleries]}>
+        {
+          showGalleries && <ShowFullGalleries assets={project.assets} />
+        }
+      </GalleryContext.Provider>
+    </>
   )
 }
 
---
+++ frontend/src/components/showFullGalleries.jsx
@@ -0,0 +1,97 @@
+'use client'
+import React, { useState, useContext } from 'react'
+import Image from 'next/image';
+import './styles/showFullGalleries.css';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+
+import { GalleryContext } from '@/app/projects/[slug]/content/page'
+
+
+
+const ShowFullGalleries = ({ assets }) => {
+  const [showGalleries, setShowGalleries] = useContext(GalleryContext)
+  const [activeImage, setActiveImage] = useState(0);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  function handlePointerImage(e){
+    e.stopPropagation();
+  }
+
+  function handleActiveImage(swiper) {
+    setActiveImage(swiper.activeIndex);
+  }
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 z-10" onClick={() => setShowGalleries(false)}>
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <div className="flex w-10/12 lg:w-7/12 z-30" onClick={handlePointerImage}>
+          <Swiper
+            loop={true}
+            modules={[Navigation, Thumbs, FreeMode]}
+            spaceBetween={10}
+            slidesPerView={1}
+            navigation
+            thumbs={{ swiper: thumbsSwiper }}
+            className="main-image rounded-xl"
+            onSlideChange={handleActiveImage}
+          >
+            {assets.map((asset, index) => (
+              <SwiperSlide key={index}>
+                <Image
+                  src={asset}
+                  alt={`image ${index}`}
+                  width={1920}
+                  height={1080}
+                  layout="cover"
+                  className="h-full w-full object-cover rounded-xl"
+                  loading="lazy"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="flex w-11/12 h-24 md:h-32 lg:w-10/12 lg:h-52 justify-center items-center mt-8" onClick={handlePointerImage}>
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              loop={true}
+              spaceBetween={20}
+              slidesPerView={
+                assets.length > 4 ? 4 : assets.length
+              }
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Thumbs, Navigation]}
+              className="thumb-image w-full h-full"
+            >
+              {assets.map((asset, index) => {
+                console.log(activeImage, index)
+                return (
+                  <SwiperSlide key={index}>
+                    <Image
+                      src={asset}
+                      alt={`image ${index}`}
+                      width={1920}
+                      height={1080}
+                      layout="cover"
+                      className={`h-full w-full object-cover rounded-lg`}
+                      loading="lazy"
+                    />
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </div>
+      </div>
+    </div>
+  )
+
+}
+
+export default ShowFullGalleries
