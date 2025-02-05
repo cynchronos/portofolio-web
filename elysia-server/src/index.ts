9b2dfa2ff18d21e0894a3de79e7547885874a14f
Kuro Cybria <rizqikurniawan1863@gmail.com>
2025-02-05 21:23:02 +0700

    change code to request from endpoint api

--- elysia-server/src/index.ts
+++ elysia-server/src/index.ts
@@ -4,13 +4,14 @@ import cors from "@elysiajs/cors";
 import mainRoutes from "./routes/main.routes";
 import { logger } from "@bogeychan/elysia-logger";
 
+const client_uri = process.env.APP_ENV === "production" ? process.env.CLIENT_URL_PROD : process.env.CLIENT_URL_DEV;
 const port = process.env.PORT || 3000;
+
 
 const app = new Elysia()
   .use(cors(
     {
-      // origin: process.env.CLIENT_URL || '*',
-      origin: '*',
+      origin: client_uri,
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
       allowedHeaders: ["Content-Type", "Authorization, X-Requested-With, X-HTTP-Method-Override, Accept, Origin"],
     }
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiUrl = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://192.168.0.33:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL_PROD : process.env.NEXT_PUBLIC_API_URL_DEV,
 });
 
 apiUrl.defaults.timeout = 10000;  
